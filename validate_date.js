1816c05b467ece72e6e5420f9245e72daf990869
Tomaayna <ayu.nobuta@gmail.com>
2025-05-22 15:42:18 +0900

    Prevent field errors when date fields missing

--- validate_date.js
+++ validate_date.js
@@ -5,8 +5,15 @@
 
   kintone.events.on(submitEvents, function (event) {
     const r = event.record;
-    const start = r.開始日.value;
-    const end   = r.終了日.value;
+    const startField = r.開始日;
+    const endField   = r.終了日;
+
+    if (!startField || !endField) {
+      return event; // Avoid runtime errors if field codes are wrong
+    }
+
+    const start = startField.value;
+    const end   = endField.value;
 
     if (start && end && new Date(start) > new Date(end)) {
  //     r.終了日.error = "終了日は開始日以降の日付を指定してください"; // 問題箇所にエラー表示(フィールド幅で改行される)
